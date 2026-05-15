644430edf667cae3867e34dd664acafa1aa2b202
thomas08095 <thomas08095@gmail.com>
2026-05-14 20:33:41 -0500

    tablero y funcion que lo imprime por consola

--- src/co/edu/unbosque/model/Tablero.java
+++ src/co/edu/unbosque/model/Tablero.java
@@ -1,5 +1,23 @@
 package co.edu.unbosque.model;
 
 public class Tablero {
+        int[][] tablero = new int[10][10];
+        public Tablero(int filas, int columnas) {
+            this.tablero = new int[filas][columnas];
+        }
 
+        public int[][] getMatriz() {
+            return tablero;
+        }       
+	public void imprimrMatriz() {
+        for (int i = 0; i<10;i++) {
+            for (int j = 0; j < tablero[i].length; j++) {
+                System.out.print(tablero[i][j] + " ");
+            }
+            System.out.println();
+        }
+	}
 }
+        
+
+
