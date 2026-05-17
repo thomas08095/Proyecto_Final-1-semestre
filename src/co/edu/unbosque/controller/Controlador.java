9ad147572e8ec79fad6b0aa1df0c4cc5d128c305
thomas08095 <thomas08095@gmail.com>
2026-05-17 11:08:59 -0500

    Creación de Ventana Principal

--- src/co/edu/unbosque/controller/Controlador.java
+++ src/co/edu/unbosque/controller/Controlador.java
@@ -1,5 +1,15 @@
 package co.edu.unbosque.controller;
 
+import co.edu.unbosque.view.VentanaPrincipal;
+
 public class Controlador {
+	private VentanaPrincipal ventana;
+	
+	public Controlador() {
+		ventana = new VentanaPrincipal();
+	}
+
+	
+	
 
 }
--- src/co/edu/unbosque/view/VentanaPrincipal.java
+++ src/co/edu/unbosque/view/VentanaPrincipal.java
@@ -1,5 +1,23 @@
 package co.edu.unbosque.view;
 
-public class VentanaPrincipal {
+import java.awt.BorderLayout;
+import java.awt.Color;
+
+import javax.swing.JFrame;
+
+public class VentanaPrincipal extends JFrame{
+	
+	public VentanaPrincipal() {
+		setTitle("Cyber Infiltrator");
+		setSize(900,600 );
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		getContentPane().setBackground(Color.LIGHT_GRAY);
+		getContentPane().setLayout(new BorderLayout(10,10));
+
+		setResizable(false);
+		setLocationRelativeTo(null);
+		setVisible(true);
+	}
+	
 
 }
