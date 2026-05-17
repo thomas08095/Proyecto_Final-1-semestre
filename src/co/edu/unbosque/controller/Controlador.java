013005ccb67b1d47dde180013ed572fba69ce6c2
thomas08095 <thomas08095@gmail.com>
2026-05-17 12:46:39 -0500

    Creacion de  Ventana Emergente

--- src/co/edu/unbosque/controller/Controlador.java
+++ src/co/edu/unbosque/controller/Controlador.java
@@ -1,12 +1,13 @@
 package co.edu.unbosque.controller;
-
 import co.edu.unbosque.view.VentanaPrincipal;
 
 public class Controlador {
 	private VentanaPrincipal ventana;
+
 	
 	public Controlador() {
 		ventana = new VentanaPrincipal();
+
 	}
 
 	
--- src/co/edu/unbosque/view/VentanaEmergente.java
+++ src/co/edu/unbosque/view/VentanaEmergente.java
@@ -1,5 +1,32 @@
 package co.edu.unbosque.view;
+import javax.swing.JOptionPane;
 
 public class VentanaEmergente {
+	
+	
+	public VentanaEmergente() {
+		
+		}
+		
+		public void mostrarInformacion(String m) {
+			JOptionPane.showMessageDialog(null, m);
+		}
+		
+		public int leerInt(String m) {
+			String aux = JOptionPane.showInputDialog(m);
+			int dato = Integer.parseInt(aux);
+			return dato;
+		}
+		
+		public String leerString(String m) {
+			String aux = JOptionPane.showInputDialog(m);
+			return aux;
+		}
+		
+		public long leerLong(String m) {
+			String aux = JOptionPane.showInputDialog(m);
+			long dato = Long.parseLong(aux);
+			return dato;
+		}
 
 }
